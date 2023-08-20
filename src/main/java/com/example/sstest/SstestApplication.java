e475e3e95aa5ddaaf01bffc1072006be8d91bf40
손민혁 <sonmh79@naver.com>
2023-08-21 02:00:45 +0900

    initialize spring boot project

---
+++ src/main/java/com/example/sstest/SstestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SstestApplication.class, args);
+	}
+
+}
